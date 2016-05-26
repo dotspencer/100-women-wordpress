bcca72767a583cabb789bf7e93f340d9a53d1244
Spencer Smith <dontaskspencer@gmail.com>
2016-05-25 23:25:42 -0600

    Sass working now

--- wp-content/themes/women-who-care/footer.php
+++ wp-content/themes/women-who-care/footer.php
@@ -15,9 +15,8 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'women-who-care' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'women-who-care' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'women-who-care' ), 'women-who-care', '<a href="http://spencerlarry.com" rel="designer">Spencer Smith</a>' ); ?>
+			<span class="sep">&copy; <?php echo date("Y") ?></span>
+			<!--<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'women-who-care' ), 'women-who-care', '<a href="http://spencerlarry.com" rel="designer">Spencer Smith</a>' ); ?>-->
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
